c40e0923284d8bfc69237e4cb0f5bc1da682b00f
allan frança dutram <allanfd12@gmail.com>
2021-01-14 16:43:02 -0300

    introdução ao next js, link entre paginas

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import Link from 'next/link';
+
 function Home() {
-    return <div>Home 3</div>
+    return (
+        <div>
+            <h1>Homepage</h1>
+            <Link href="/sobre">
+                <a>Ir para Sobre</a>
+            </Link>
+        </div>
+        )
 }
 
 export default Home
